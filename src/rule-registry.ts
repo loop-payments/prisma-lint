7a990929f7fc4e5017cfcdf333d0eca553533570
Max Heinritz <max@loop.com>
2023-01-15 21:33:12 -0600

    s

--- src/rule-registry.ts
+++ src/rule-registry.ts
@@ -1,5 +1,8 @@
 import singularModelName from "src/rules/singular-model-name";
+import dbPrefixedModelName from "src/rules/db-prefixed-model-name";
 
+// TODO: use prisma-lint.json for this.
 export default {
+  "db-prefixed-model-name": dbPrefixedModelName,
   "singular-model-name": singularModelName,
 };
