b541796f184934bdc0957ea1a2899c302201f0d5
Max Heinritz <max@loop.com>
2023-05-29 20:10:17 -0500

    finish set version

--- scripts/set-version.js
+++ scripts/set-version.js
@@ -1,12 +1,8 @@
 import fs from 'fs';
 import { execSync } from 'child_process';
 
-// Check if the --dry-run option is specified
-const dryRun = process.argv.includes('--dry-run');
-
 // Check if the new version is provided as a command-line argument
-const newVersionIndex = process.argv.indexOf('--dry-run') === -1 ? 2 : 3;
-const newVersion = process.argv[newVersionIndex];
+const newVersion = process.argv[2];
 if (!newVersion) {
   console.error(
     'Error: Please provide the new version as a command-line argument.',
@@ -20,16 +16,9 @@ const currentVersion = packageJson.version;
 
 // Check if the new version is the same as the current version
 if (newVersion === currentVersion) {
-  if (dryRun) {
-    console.error(
-      'Unexpected! Running in dry run mode. The new version is the same as the current version.',
-    );
-    process.exit(1);
-  } else {
-    console.log(
-      'The new version is the same as the current version. Skipping the release process.',
-    );
-  }
+  console.log(
+    'The new version is the same as the current version. Skipping the release process.',
+  );
 } else {
   // Update package.json with the new version
   packageJson.version = newVersion;
@@ -55,31 +44,5 @@ if (newVersion === currentVersion) {
       `## Unreleased\n\n${changelogHeader}`,
     );
     fs.writeFileSync(changelogPath, changelog);
-  }
-
-  // Perform the changes only if it's not a dry run
-  if (!dryRun) {
-    try {
-      // Check if the current branch has an upstream branch set
-      execSync('git rev-parse --abbrev-ref --symbolic-full-name @{u}');
-    } catch {
-      // Set the upstream branch
-      execSync(`git push --set-upstream origin ${currentBranch()}`);
-    }
-
-    // Commit and push the changes to Git
-    execSync('git add package.json CHANGELOG.md');
-    execSync(`git commit -m "Bump version to ${newVersion}"`);
-    execSync('git push');
-
-    // Publish a new release on GitHub
-    execSync(`gh release create ${newVersion} --notes-file CHANGELOG.md`);
-
-    // Publish the npm package
-    execSync('npm publish');
-  } else {
-    console.log('Dry run mode. Changes made locally:');
-    console.log(`- Updated package.json version to ${newVersion}`);
-    console.log(`- Added a new version section in ${changelogPath}`);
   }
 }
