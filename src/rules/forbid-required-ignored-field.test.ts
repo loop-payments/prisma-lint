40556b0f00ff5ef1e081f59338410814c0fd6dea
Will Taft <will@loop.us>
2023-11-01 16:40:03 -0500

    fixup

--- src/rules/forbid-required-ignored-field.test.ts
+++ src/rules/forbid-required-ignored-field.test.ts
@@ -52,32 +52,32 @@ describe('forbid-required-ignored-field', () => {
           expect(violations.length).toEqual(1);
         });
       });
+    });
 
-      describe('allowIgnoreWithDefault configuration', () => {
-        describe('allowIgnoreWithDefault false', () => {
-          const run = getRunner({ allowIgnoreWithDefault: false });
-          it('returns violation for an ignored field with a default', async () => {
-            const violations = await run(`
+    describe('allowIgnoreWithDefault configuration', () => {
+      describe('allowIgnoreWithDefault false', () => {
+        const run = getRunner({ allowIgnoreWithDefault: false });
+        it('returns violation for an ignored field with a default', async () => {
+          const violations = await run(`
       model Users {
         id String
         toBeRemoved String @ignore @default(false)
       }
     `);
-            expect(violations.length).toEqual(1);
-          });
+          expect(violations.length).toEqual(1);
         });
+      });
 
-        describe('allowIgnoreWithDefault true', () => {
-          const run = getRunner({ allowIgnoreWithDefault: true });
-          it('returns no violations for an ignored field with a default', async () => {
-            const violations = await run(`
+      describe('allowIgnoreWithDefault true', () => {
+        const run = getRunner({ allowIgnoreWithDefault: true });
+        it('returns no violations for an ignored field with a default', async () => {
+          const violations = await run(`
       model Users {
         id String
         toBeRemoved String @ignore @default(false)
       }
     `);
-            expect(violations.length).toEqual(0);
-          });
+          expect(violations.length).toEqual(0);
         });
       });
     });
