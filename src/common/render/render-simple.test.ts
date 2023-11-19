2c4e484c92eb91701de9231d32bacecb80f62057
Max Heinritz <max@loop.com>
2023-11-19 15:43:11 -0600

    add more snapshot tests

--- src/common/render/render-simple.test.ts
+++ src/common/render/render-simple.test.ts
@@ -1,8 +1,6 @@
 
-import { renderViolationsContextual } from '#src/common/render/render-contextual.js';
-import { renderViolationsJsonObject } from '#src/common/render/render-json.js';
 import { renderViolationsSimple } from '#src/common/render/render-simple.js';
-import { MOCK_SOURCE_CODE, MOCK_VIOLATIONS } from '#src/common/render/render-test-util.js';
+import { MOCK_VIOLATIONS } from '#src/common/render/render-test-util.js';
 
 describe('render json', () => {
   it('matches snapshot', () => {
