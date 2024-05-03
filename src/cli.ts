21e1cd2d4548bbae79b2a21366927cfb82352793
Max Heinritz <max@loop.com>
2024-05-03 15:26:17 -0500

    fix in a loop

--- src/cli.ts
+++ src/cli.ts
@@ -8,15 +8,11 @@ import { program } from 'commander';
 import { cosmiconfig } from 'cosmiconfig';
 import { glob } from 'glob';
 
-import pluralize from 'pluralize';
 import { readPackageUp } from 'read-package-up';
 
 import { getTruncatedFileName } from '#src/common/file.js';
-import {
-  applyFixesToPrismaSchema,
-  overwriteSourceCode,
-} from '#src/common/fix.js';
 import { parseRules } from '#src/common/parse-rules.js';
+import { fixFilesUntilStable as fixPrismaFilesUntilStable } from '#src/fix/fix.js';
 import { lintPrismaFiles } from '#src/lint-prisma-files.js';
 import { outputToConsole } from '#src/output/console.js';
 import ruleDefinitions from '#src/rule-definitions.js';
@@ -134,34 +130,16 @@ const run = async () => {
   }
 
   const fileNames = await resolvePrismaFiles(args);
-  const fileResults = await lintPrismaFiles({
-    rules,
-    fileNames,
-  });
-
-  outputToConsole(fileResults, outputFormat, quiet);
+  const input = { rules, fileNames };
+  const fileResults = options.fix
+    ? await fixPrismaFilesUntilStable(input)
+    : await lintPrismaFiles(input);
 
   const hasViolations = fileResults.some(
     ({ violations }) => violations.length > 0,
   );
   if (hasViolations) {
-    if (options.fix) {
-      // eslint-disable-next-line no-console
-      console.log('\nAttempting to automatically fix violations...');
-      for (const fileResult of fileResults) {
-        const fixedCount = applyFixesToPrismaSchema(fileResult);
-        if (fixedCount > 0) {
-          await overwriteSourceCode(fileResult);
-        }
-        // eslint-disable-next-line no-console
-        console.log(
-          `Fixed ${fixedCount} ${pluralize('violations', fixedCount)} in ${
-            fileResult.fileName
-          }`,
-        );
-      }
-      // TODO - run in a loop until there are no violations?
-    }
+    outputToConsole(fileResults, outputFormat, quiet);
     process.exit(1);
   }
 };
