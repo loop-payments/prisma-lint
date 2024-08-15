9f4ec7d8bfbded4e8850a63e199fd5bea0041439
Andy Young <andy@apexa.co.uk>
2024-08-15 19:37:00 +0100

    Add new rules `model-name-pascal-case` and `field-name-camel-case` (#458)
    Hopefully self-explanatory(!)

--- src/rule-definitions.ts
+++ src/rule-definitions.ts
@@ -1,10 +1,12 @@
 import type { RuleDefinition } from '#src/common/rule.js';
+import fieldNameCamelCase from '#src/rules/field-name-camel-case.js';
 import fieldNameMappingSnakeCase from '#src/rules/field-name-mapping-snake-case.js';
 import fieldOrder from '#src/rules/field-order.js';
 import forbidField from '#src/rules/forbid-field.js';
 import forbidRequiredIgnoredField from '#src/rules/forbid-required-ignored-field.js';
 import modelNameGrammaticalNumber from '#src/rules/model-name-grammatical-number.js';
 import modelNameMappingSnakeCase from '#src/rules/model-name-mapping-snake-case.js';
+import modelNamePascalCase from '#src/rules/model-name-pascal-case.js';
 import modelNamePrefix from '#src/rules/model-name-prefix.js';
 import requireDefaultEmptyArrays from '#src/rules/require-default-empty-arrays.js';
 import requireFieldIndex from '#src/rules/require-field-index.js';
@@ -12,12 +14,14 @@ import requireFieldType from '#src/rules/require-field-type.js';
 import requireField from '#src/rules/require-field.js';
 
 export default [
+  fieldNameCamelCase,
   fieldNameMappingSnakeCase,
   fieldOrder,
   forbidField,
   forbidRequiredIgnoredField,
   modelNameGrammaticalNumber,
   modelNameMappingSnakeCase,
+  modelNamePascalCase,
   modelNamePrefix,
   requireDefaultEmptyArrays,
   requireField,
